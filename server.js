ce6d83b6fb76a1b6a00ba268ba686ace6d1762d3
Jeffrey Patajo <jpatajo@gmail.com>
2021-02-13 20:15:07 -0800

    Added bootcamps route file

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load environment variables "config.env"
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 // Initialize express
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
