6fdf03e7b4d3ac185ee1d5c19d0b14da16fbc5da
Jeffrey Patajo <jpatajo@gmail.com>
2021-02-21 17:37:19 -0800

    Added slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -98,6 +99,12 @@ const BootcampSchema = new mongoose.Schema({
       default: Date.now,
     },
   },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true }); // This uses slugify to modify the name. "this.slug" can be called from within schema
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
