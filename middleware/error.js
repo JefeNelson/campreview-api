1a44f55fafc3291890ca82a0dcde39e7a93b3301
Jeffrey Patajo <jpatajo@gmail.com>
2021-02-21 17:24:49 -0800

    Added async handler for DRY method

--- middleware/error.js
+++ middleware/error.js
@@ -6,6 +6,8 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for dev
+  //console.log(err);
+
   console.log(err.stack.red);
   console.log(`Wrong ID value: ${err.value}`);
   console.log(`Error name: ${err.name}`);
@@ -16,6 +18,19 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
+  // Mongoose Validation Error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose duplicate key (Bootcamp name duplicate)
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Default error output if the previous three are not triggered
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error',
