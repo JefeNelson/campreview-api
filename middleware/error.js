d2cb947dd85aff2d616ffaabb7deea892314f681
Jeffrey Patajo <jpatajo@gmail.com>
2021-02-21 16:14:54 -0800

    Incorporated error handler in Bootcamp controller

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,24 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
+
   // Log to console for dev
   console.log(err.stack.red);
+  console.log(`Wrong ID value: ${err.value}`);
+  console.log(`Error name: ${err.name}`);
 
-  res.status(err.statusCode || 500).json({
+  // Mongoose bad ObjectId (If there is a Cast error, message completely changes)
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || 'Server Error',
+    error: error.message || 'Server Error',
   });
 };
 
